84ffec8ef93ddb4ecaf7b2e8f52c3835bbf3ef7c
DANIEL WILLIAMS MENSAH <83681534+Williams9007@users.noreply.github.com>
2026-09-23 05:50:12 -0700

    chore: bump virtualclass plugin version to trigger Moodle upgrade + cache purge

--- moodle-sso/local/studiesmasters_virtualclass/version.php
+++ moodle-sso/local/studiesmasters_virtualclass/version.php
@@ -11,7 +11,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_studiesmasters_virtualclass';
-$plugin->version   = 2026090800; // YYYYMMDDXX
+$plugin->version   = 2026090801; // YYYYMMDDXX
 $plugin->requires  = 2020061500; // Moodle 3.9+
 $plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
